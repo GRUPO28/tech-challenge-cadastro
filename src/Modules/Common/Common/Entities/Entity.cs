1fce84cb3c6ebcff5c09b13cb3219a5091a30f76
ALCM-bit <falcm-bit@outlook.com>
2024-10-17 21:11:05 -0300

    Montando configurações do Domain
    Passando confgurações do Domain para novo repo

---
+++ src/Modules/Common/Common/Entities/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Entities;
+
+public abstract class Entity(string id, DateTime dataCriacao)
+{
+    public string Id { get; } = id;
+    public DateTime DataCriacao { get; } = dataCriacao;
+    public DateTime? DataAtualizacao { get; set; }
+
+    protected abstract void Validate();
+}
